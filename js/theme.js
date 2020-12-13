806457ac59628a20ece9aca9a3adb7e35863112c
Zachary Ferretti <zf66728@gmail.com>
2020-12-12 21:23:32 -0500

    Beginning Customizatoin.

--- js/theme.js
+++ js/theme.js
@@ -33,13 +33,12 @@ themeToggle.addEventListener('click', () => {
 
 // Change the theme with the time
 
-// const today = new Date();
-// const Hr = today.getHours();
+ const today = new Date();
+ const Hr = today.getHours();
 
-/*
-if (Hr >= 19 || Hr < 5) {
+
+if (Hr >= 18 || Hr < 5) {
     enableDark();
 } else {
     disableDark();
 }
-*/
--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = '7185f138361a5b9c08968a4b7d0ac06e';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = 42.367110;
+    let longitude = -71.054080;
 
     getWeather(latitude, longitude);
 }
