f47244deb14daeb825e8e02b181c52c9e6a50b02
Zachary Ferretti <zf66728@gmail.com>
2021-01-02 13:14:00 -0500

    Updating Personalization

--- js/theme.js
+++ js/theme.js
@@ -1,44 +1,43 @@
 // Store the theme
-let darkTheme = localStorage.getItem('darkTheme');
-const themeToggle = document.querySelector('#themeButton');
+let darkTheme = localStorage.getItem("darkTheme");
+const themeToggle = document.querySelector("#themeButton");
 
 const enableDark = () => {
-    document.body.classList.add('darktheme');
-    localStorage.setItem('darkTheme', 'enabled');
-    themeToggle.innerHTML = `<i id="bicon" data-feather="moon"></i>`;
-    feather.replace();
+  document.body.classList.add("darktheme");
+  localStorage.setItem("darkTheme", "enabled");
+  themeToggle.innerHTML = `<i id="bicon" data-feather="moon"></i>`;
+  feather.replace();
 };
 
 const disableDark = () => {
-    document.body.classList.remove('darktheme');
-    localStorage.setItem('darkTheme', null);
-    themeToggle.innerHTML = `<i id="bicon" data-feather="sun"></i>`;
-    feather.replace();
+  document.body.classList.remove("darktheme");
+  localStorage.setItem("darkTheme", null);
+  themeToggle.innerHTML = `<i id="bicon" data-feather="sun"></i>`;
+  feather.replace();
 };
 
-if (darkTheme === 'enabled') {
-    enableDark();
+if (darkTheme === "enabled") {
+  enableDark();
 } else {
-    disableDark();
+  disableDark();
 }
 
-themeToggle.addEventListener('click', () => {
-    darkTheme = localStorage.getItem('darkTheme');
-    if (darkTheme !== 'enabled') {
-        enableDark();
-    } else {
-        disableDark();
-    }
+themeToggle.addEventListener("click", () => {
+  darkTheme = localStorage.getItem("darkTheme");
+  if (darkTheme !== "enabled") {
+    enableDark();
+  } else {
+    disableDark();
+  }
 });
 
 // Change the theme with the time
 
- const today = new Date();
- const Hr = today.getHours();
+const today = new Date();
+const Hr = today.getHours();
 
-
-if (Hr >= 18 || Hr < 5) {
-    enableDark();
+if (Hr >= 14 || Hr < 5) {
+  enableDark();
 } else {
-    disableDark();
+  disableDark();
 }
