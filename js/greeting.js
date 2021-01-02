391dcb376229a15619b09f8fdbac58d08cb86d93
Zachary Ferretti <zf66728@gmail.com>
2021-01-02 13:14:00 -0500

    Updating Personalization

--- js/greeting.js
+++ js/greeting.js
@@ -3,23 +3,24 @@ var today = new Date();
 var hour = today.getHours();
 
 // Here you can change your name
-var name = 'Zach';
+var name = "Zach";
 
 // Here you can change your greetings
-var gree1 = 'Go to Sleep! ';
-var gree2 = 'Good morning! ';
-var gree3 = 'Good afternoon ';
-var gree4 = 'Good evening ';
-var gree5 = 'Good evening ';
-var gree6 = 'Good evening ';
+var gree1 = "Go to Sleep! ";
+var gree2 = "Good morning! ";
+var gree3 = "Good afternoon ";
+var gree4 = "Good evening ";
+var gree5 = "Good evening ";
+var gree6 = "Good evening ";
 
 // Define the hours of the greetings
-if (hour >= 23 && hour < 5) {
-    document.getElementById('greetings').innerText = gree1 + name;
+document.getElementById("greetings").innerText = "Welcome home Zach.";
+/*if (hour >= 23 && hour < 5) {
+  document.getElementById("greetings").innerText = gree1 + name;
 } else if (hour >= 6 && hour < 12) {
-    document.getElementById('greetings').innerText = gree2 + name;
+  document.getElementById("greetings").innerText = gree2 + name;
 } else if (hour >= 12 && hour < 17) {
-    document.getElementById('greetings').innerText = gree3 + name;
-} else  {
-    document.getElementById('greetings').innerText = gree4 + name;
-}
+  document.getElementById("greetings").innerText = gree3 + name;
+} else {
+  document.getElementById("greetings").innerText = gree4 + name;
+}*/
--- js/weather.js
+++ js/weather.js
@@ -1,57 +1,58 @@
-const iconElement = document.querySelector('.weather-icon');
-const tempElement = document.querySelector('.temperature-value p');
-const descElement = document.querySelector('.temperature-description p');
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
 
 // App data
 const weather = {};
 weather.temperature = {
-    unit: 'celsius',
+  unit: "celsius",
 };
 
 // Change to 'F' for Fahrenheit
-var tempUnit = 'C';
+var tempUnit = "F";
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = '7185f138361a5b9c08968a4b7d0ac06e';
+const key = "7185f138361a5b9c08968a4b7d0ac06e";
 
 // Set Position function
 setPosition();
 
 function setPosition(position) {
-    // Here you can change your position
-    // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 42.367110;
-    let longitude = -71.054080;
+  // Here you can change your position
+  // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
+  let latitude = 42.36711;
+  let longitude = -71.05408;
 
-    getWeather(latitude, longitude);
+  getWeather(latitude, longitude);
 }
 
 // Get the Weather data
 function getWeather(latitude, longitude) {
-    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+  let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
-    console.log(api);
+  console.log(api);
 
-    fetch(api)
-        .then(function (response) {
-            let data = response.json();
-            return data;
-        })
-        .then(function (data) {
-            let celsius = Math.floor(data.main.temp - KELVIN);
-            weather.temperature.value = (tempUnit == 'C') ? celsius : (celsius * 9/5) + 32;
-            weather.description = data.weather[0].description + " in Boston.";
-            weather.iconId = data.weather[0].icon;
-        })
-        .then(function () {
-            displayWeather();
-        });
+  fetch(api)
+    .then(function (response) {
+      let data = response.json();
+      return data;
+    })
+    .then(function (data) {
+      let celsius = Math.floor(data.main.temp - KELVIN);
+      weather.temperature.value =
+        tempUnit == "C" ? celsius : (celsius * 9) / 5 + 32;
+      weather.description = data.weather[0].description + " in Boston.";
+      weather.iconId = data.weather[0].icon;
+    })
+    .then(function () {
+      displayWeather();
+    });
 }
 
 // Display Weather info
 function displayWeather() {
-    iconElement.innerHTML = `<img src="icons/OneDark/${weather.iconId}.png"/>`;
-    tempElement.innerHTML = `${weather.temperature.value}°<span class="darkfg">${tempUnit}</span>`;
-    descElement.innerHTML = weather.description;
+  iconElement.innerHTML = `<img src="icons/OneDark/${weather.iconId}.png"/>`;
+  tempElement.innerHTML = `${weather.temperature.value}°<span class="darkfg">${tempUnit}</span>`;
+  descElement.innerHTML = weather.description;
 }
